4372d95b1598354d2753284333e5b3377e613bcc
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-04-11 21:41:15 +1000

    feat: complete styling base header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,7 @@
-import { useEffect, useState } from 'react'
 
+import Header from "./components/Header/Header"
 export default function App() {
-    const [message, setMessage] = useState('Loading...')
-
-    useEffect(() => {
-        fetch('/api/health')
-            .then((res) => res.json())
-            .then((data) => setMessage(JSON.stringify(data, null, 2)))
-            .catch((err) => setMessage(String(err)))
-    }, [])
-
-    return (
-        <div style={{ padding: 24 }}>
-            <h1>My App</h1>
-            <pre>{message}</pre>
-        </div>
-    )
+    return <>
+        <Header />
+    </>
 }
