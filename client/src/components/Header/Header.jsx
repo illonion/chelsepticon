08114987862087d8cc5084dc42700b01dac5bef9
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2026-04-12 15:28:21 +1000

    fix: adjust logos and add in logo-min

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react" // Add this
+import { useState } from "react"
+import { Link, NavLink } from "react-router-dom"
+
+import HeaderNav from "./HeaderNav"
+import HeaderHamburger from "./HeaderHamburger"
+
 import "./Header.css"
 import logoFull from "../../assets/logo.png"
-import HeaderNav from "./HeaderNav"
-import { Link, NavLink } from "react-router-dom"
-import HeaderHamburger from "./HeaderHamburger"
+import logoMin from "../../assets/logo-min.png"
 
 export default function Header() {
     const webpageList = ["home", "about", "services", "portfolio", "contact"]
@@ -19,9 +22,16 @@ export default function Header() {
             <div className="header-content">
                 <Link to="/">
                     <img
+                        id="header-logo-full"
                         className="header-image"
                         src={logoFull}
                         alt="Full Logo"
+                    />
+                    <img
+                        id="header-logo-mini"
+                        className="header-image"
+                        src={logoMin}
+                        alt="Mini Logo"
                     />
                 </Link>
                 <HeaderNav webpageList={webpageList} />
